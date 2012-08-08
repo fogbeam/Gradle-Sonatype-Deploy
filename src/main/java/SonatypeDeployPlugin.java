cec2d70b4459be5e9018c95ceb203720a95439b8
Robert Fischer <rfischer@reverbnation.com>
2012-08-08 15:18:55 -0400

    Add jar to the archives

--- src/main/java/SonatypeDeployPlugin.java
+++ src/main/java/SonatypeDeployPlugin.java
@@ -6,8 +6,12 @@ import com.google.common.collect.ImmutableMap;
 public class SonatypeDeployPlugin implements Plugin<Project> {
 
   public void apply(Project target) {
+    // TODO Add a plugin to generate a javadoc and a source jar.
     target.apply(ImmutableMap.of("apply", "maven"));
     target.apply(ImmutableMap.of("apply", "signing"));
+    target.getArtifacts().add("archives", "jar");
   }
+
+
 
 }
